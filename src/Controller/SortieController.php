26e37a94c7497f8ae9ef42881df2e312df892cc7
Clément Mze <136915029+clementmze@users.noreply.github.com>
2025-10-20 12:06:05 +0200

    Feat: ajout de SprtieController

---
+++ src/Controller/SortieController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/sorties', name: 'sorties_')]
+final class SortieController extends AbstractController
+{
+    #[Route('/', name: 'list')]
+    public function index(): Response
+    {
+        return $this->render('sortie/index.html.twig', [
+            'controller_name' => 'SortieController',
+        ]);
+    }
+}
