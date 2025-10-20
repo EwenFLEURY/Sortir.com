1cd13cdefa283e1642c065f189559cb38f52f318
Clément Mze <136915029+clementmze@users.noreply.github.com>
2025-10-20 15:49:51 +0200

    Feat: Add list sorties

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,17 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/sorties', name: 'sorties_')]
 final class SortieController extends AbstractController
 {
-    #[Route('/', name: 'index', methods: ['GET'])]
-    public function index(): Response
+    public function __construct(
+        private readonly SortieRepository $sortieRepository,
+    ) {
+    }
+    #[Route('/', name: 'list', methods: ['GET','POST'])]
+    public function list(): Response
     {
+        $sorties = $this->sortieRepository->findAll();
         return $this->render('sortie/sorties.html.twig', [
+            'sorties' => $sorties,
         ]);
     }
+
 }
