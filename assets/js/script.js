447642e677a808ff1c3e565a974cd65aaf04ab71
EwenFLEURY <ew.fleury@gmail.com>
2025-10-20 16:47:40 +0200

    fix: base.html

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Ajout un évènement de retour en arrière à tous les boutons avec la class goback
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".goback").forEach((el) => {
+        el.addEventListener("click", (e) => {
+            e.preventDefault(); // évite la navigation si c’est un <a>
+            window.history.back();
+        });
+    });
+});
+
